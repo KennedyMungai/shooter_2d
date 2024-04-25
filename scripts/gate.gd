5368593fa5e088d5ec941176d078a0ad535b4682
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 14:58:47 +0300

    Added a body parameter to the player_entered_gate signal

--- scripts/gate.gd
+++ scripts/gate.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-signal player_entered_gate
+signal player_entered_gate(body)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	player_entered_gate.emit()
+	player_entered_gate.emit(body)
