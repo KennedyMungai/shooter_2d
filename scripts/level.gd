dd3f40ce7a55e4de2fe10d579a351662edb5ea3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 15:02:31 +0300

    Connected the laser_fired signal from the player

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,3 +4,7 @@ extends Node2D
 func _on_gate_player_entered_gate(body) -> void:
 	print('Player has entered gate external')
 	print(body)
+
+
+func _on_player_laser_fired() -> void:
+	print("Laser has been fired external")
