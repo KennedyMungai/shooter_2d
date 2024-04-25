f64f6ee91fb3a55d9108f71be39098b06ffb6970
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 09:38:08 +0300

    Added a simple for loop to print out an array of strings

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+var test_array: Array[String] = ['Something', 'in', 'the', 'way']
 
 func _ready() -> void:
 	print('The level is ready')
+	
+	for i in test_array:
+		print(i)
 
 
-func _process(delta: float) -> void:
-	print('I have a big grown male chicken')
+#func _process(delta: float) -> void:
+	#print('I have a big grown male chicken')
