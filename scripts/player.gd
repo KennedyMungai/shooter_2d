55bd0d178e32f32b6927c3d630e922e286a08f7e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 10:12:14 +0300

    Added a const to old the player speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const PLAYER_SPEED: float = 500
+
 func _process(delta: float) -> void:
 	var direction = Input.get_vector('left', 'right', 'up','down')
-	position += direction * 500 * delta
+	position += direction * PLAYER_SPEED * delta
