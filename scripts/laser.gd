c094e3dbea6dfb5d6a7faeb55ae5c7845a03cf96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 16:02:44 +0300

    Added a timer node to the scene so that the laser is automatically deleted after 4 seconds

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -8,3 +8,8 @@ var direction: Vector2 = Vector2.UP
 
 func _process(delta: float) -> void:
 	position += direction * LASER_SPEED * delta
+
+
+func _on_laser_lifespan_timeout() -> void:
+	print('To be deleted')
+	queue_free()
