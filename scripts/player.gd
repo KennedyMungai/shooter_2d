8a01d752d74980ee911f201d9a695270e9675cb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 14:42:53 +0300

    Added the logic for the shooting and grenade timers

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,10 +5,12 @@ const PLAYER_SPEED: float = 500
 var can_laser: bool = true
 var can_grenade: bool = true
 
+
 func _process(_delta: float) -> void:
 	var direction = Input.get_vector('left', 'right', 'up','down')
 	velocity = direction * PLAYER_SPEED
 	move_and_slide()
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if(event.is_action_pressed('primary action')) and can_laser:
@@ -19,6 +21,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		can_grenade = false
 
 
-func _on_timer_timeout() -> void:
+func _on_shooting_timer_timeout() -> void:
 	can_laser = true
+
+
+func _on_grenade_timer_timeout() -> void:
 	can_grenade = true
