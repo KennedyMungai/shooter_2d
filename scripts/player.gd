fc2059013d221f69ced08f1914a162d82e3c24c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 10:16:16 +0300

    Added the calls for shooting a laser and grenades to an unhandled input function

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,3 +5,10 @@ const PLAYER_SPEED: float = 500
 func _process(delta: float) -> void:
 	var direction = Input.get_vector('left', 'right', 'up','down')
 	position += direction * PLAYER_SPEED * delta
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if(event.is_action_pressed('primary action')):
+		print('Shooting a laser')
+	elif(event.is_action_pressed('secondary action')):
+		print('Shoot grenade')
