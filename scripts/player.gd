c9f5d3938fd86f41ecc7f5d4556f032e338a53e5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 14:40:19 +0300

    Added some booleans to limit the rate the player can release a shot

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,23 @@ extends CharacterBody2D
 
 const PLAYER_SPEED: float = 500
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta: float) -> void:
 	var direction = Input.get_vector('left', 'right', 'up','down')
 	velocity = direction * PLAYER_SPEED
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if(event.is_action_pressed('primary action')):
+	if(event.is_action_pressed('primary action')) and can_laser:
 		print('Shooting a laser')
-	elif(event.is_action_pressed('secondary action')):
+		can_laser = false
+	elif(event.is_action_pressed('secondary action')) and can_grenade:
 		print('Shoot grenade')
+		can_grenade = false
+
+
+func _on_timer_timeout() -> void:
+	can_laser = true
+	can_grenade = true
