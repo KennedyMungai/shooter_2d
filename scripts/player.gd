8f56218ef9b54ef796842809731d82fe3f3fdc2b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 15:49:59 +0300

    Added a prop to the laser fired signal for a value of type Vector2 and used it to send the emitter position

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,7 @@ var can_laser: bool = true
 var can_grenade: bool = true
 
 
-signal laser_fired
+signal laser_fired(emitter_position: Vector2)
 signal grenade_thrown
 
 
@@ -18,8 +18,10 @@ func _process(_delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if(event.is_action_pressed('primary action')) and can_laser:
+		var laser_markers = $laser_start_positions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size() - 1]
 		can_laser = false
-		laser_fired.emit()
+		laser_fired.emit(selected_laser.position)
 	elif(event.is_action_pressed('secondary action')) and can_grenade:
 		can_grenade = false
 		grenade_thrown.emit()
