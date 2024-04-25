05bec3695fba6e0715552a9476bf5e32eb2bf431
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-25 16:27:56 +0300

    Using the projectiles node as the parent of the laser and grenade instances

--- scripts/level.gd
+++ scripts/level.gd
@@ -13,10 +13,10 @@ func _on_gate_player_entered_gate(body) -> void:
 func _on_player_laser_fired(emitter_position) -> void:
 	var laser = laser_scene.instantiate()
 	laser.position = emitter_position
-	add_child(laser)
+	$projectiles.add_child(laser)
 
 
 func _on_player_grenade_thrown(emitter_position) -> void:
 	var grenade = grenade_scene.instantiate()
 	grenade.position = emitter_position
-	add_child(grenade)
+	$projectiles.add_child(grenade)
