20b51eeb1777b47d55c3142d1adb85720cc15095
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-26 09:58:29 +0300

    Added the direction parameter to the signal to fire lasers

--- scripts/level.gd
+++ scripts/level.gd
@@ -10,14 +10,15 @@ func _on_gate_player_entered_gate(body) -> void:
 	print(body)
 
 
-func _on_player_laser_fired(emitter_position) -> void:
-	var laser = laser_scene.instantiate()
+func _on_player_laser_fired(emitter_position, direction_fired) -> void:
+	var laser = laser_scene.instantiate() as Area2D
 	laser.position = emitter_position
+	laser.rotation_degrees = direction_fired.angle()
 	$projectiles.add_child(laser)
 
 
 func _on_player_grenade_thrown(emitter_position, direction_thrown) -> void:
-	var grenade: RigidBody2D = grenade_scene.instantiate()
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = emitter_position
 	grenade.linear_velocity = direction_thrown * grenade.GRENADE_SPEED
 	$projectiles.add_child(grenade)
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,7 @@ var can_laser: bool = true
 var can_grenade: bool = true
 
 
-signal laser_fired(emitter_position: Vector2)
+signal laser_fired(emitter_position: Vector2, fired_direction: Vector2)
 signal grenade_thrown(emitter_position: Vector2, direction: Vector2)
 
 
@@ -19,14 +19,16 @@ func _process(_delta: float) -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	var player_direction: Vector2 = (get_global_mouse_position() - position).normalized()
+	
 	if(event.is_action_pressed('primary action')) and can_laser:
 		var laser_markers = $laser_start_positions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size() - 1]
 		can_laser = false
-		laser_fired.emit(selected_laser.global_position)
+		laser_fired.emit(selected_laser.global_position, player_direction)
 	elif(event.is_action_pressed('secondary action')) and can_grenade:
 		can_grenade = false
-		grenade_thrown.emit($grenade_starter_position/grenade_starter_marker.global_position, (get_global_mouse_position() - position).normalized())
+		grenade_thrown.emit($grenade_starter_position/grenade_starter_marker.global_position, player_direction)
 
 
 func _on_shooting_timer_timeout() -> void:
