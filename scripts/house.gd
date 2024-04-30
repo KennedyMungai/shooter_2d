2aba13d3c2354f92b9f2f83ed3081e954968e715
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-30 12:24:41 +0300

    Added underscores to unused parameters

--- scripts/house.gd
+++ scripts/house.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 
-func _on_house_entered(body: Node2D) -> void:
+func _on_house_entered(_body: Node2D) -> void:
 	print('Player has entered')
 
 
-func _on_house_exited(body: Node2D) -> void:
+func _on_house_exited(_body: Node2D) -> void:
 	print('Player has exited')
