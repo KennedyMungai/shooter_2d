9ea3e393c33a12af716ead60e0de70ecc9bd2eb1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-30 12:13:05 +0300

    Removing some unnecessary TODOs from the code

--- scripts/house.gd
+++ scripts/house.gd
@@ -2,10 +2,8 @@ extends Area2D
 
 
 func _on_house_entered(body: Node2D) -> void:
-	#TODO: Add a check for the player object
 	print('Player has entered')
 
 
 func _on_house_exited(body: Node2D) -> void:
-	#TODO: Add a check for thhe player object
 	print('Player has exited')
