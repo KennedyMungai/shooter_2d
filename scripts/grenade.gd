3c856b9cae527f2d0220c16b820051dc89202b2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-30 11:13:58 +0300

    Deleting the grenade after the explosion animation is done

--- scripts/grenade.gd
+++ scripts/grenade.gd
@@ -4,3 +4,7 @@ const GRENADE_SPEED: int = 750
 
 func _ready() -> void:
 	linear_velocity = Vector2(15, 3)
+
+
+func _on_explosion_animation_animation_finished(anim_name: StringName) -> void:
+	queue_free()
