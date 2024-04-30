3dad02a401d18a3d4edddc0f6ab5dc7201f02cec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-30 12:43:42 +0300

    Tweening the alpha modulation of the player sprite

--- scripts/level.gd
+++ scripts/level.gd
@@ -28,8 +28,10 @@ func _on_player_grenade_thrown(emitter_position, direction_thrown) -> void:
 func _on_house_player_entered() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($player/Camera2D, 'zoom', Vector2(1,1), 0.6)
+	tween.tween_property($player, "modulate:a", 0.3, 0.6)
 
 
 func _on_house_player_exited() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($player/Camera2D, 'zoom', Vector2(0.6, 0.6), 0.6)
+	tween.tween_property($player, "modulate:a", 1, 0.6)
