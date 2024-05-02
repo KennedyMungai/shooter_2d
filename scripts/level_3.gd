c2a78116e189215af9668e962332fb0328b165e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-02 09:25:39 +0300

    Added an underscore to an unused parameter

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -1,6 +1,6 @@
 extends LevelParent
 
 
-func _on_doorway_body_entered(body: Node2D) -> void:
+func _on_doorway_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($player, 'player_speed', 0, 0.5)
