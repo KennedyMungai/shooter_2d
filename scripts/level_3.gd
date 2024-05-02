a58895479e66319758e45cafd4fbd5aa1526ee79
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-02 09:31:35 +0300

    Added the logic to switch scenes based on some triggers

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -4,3 +4,4 @@ extends LevelParent
 func _on_doorway_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($player, 'player_speed', 0, 0.5)
+	get_tree().change_scene_to_file('res://levels/level_2.tscn')
