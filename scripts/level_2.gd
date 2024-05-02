e11791603812bc192fdae34c7032e24541a2356f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-05-02 09:36:21 +0300

    Using packed scenes instead of the scene files

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -1,7 +1,9 @@
 extends LevelParent
 
+var inside_level: PackedScene = preload('res://levels/level_3.tscn')
+
 
 func _on_gate_player_entered_gate(_body) -> void:
 	var tween = create_tween()
 	tween.tween_property($player, "player_speed", 0, 0.5)
-	get_tree().change_scene_to_file('res://levels/level_3.tscn')
+	get_tree().change_scene_to_packed(inside_level)
--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -1,7 +1,10 @@
 extends LevelParent
 
 
+var outside_level: PackedScene = preload('res://levels/level_2.tscn')
+
+
 func _on_doorway_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($player, 'player_speed', 0, 0.5)
-	get_tree().change_scene_to_file('res://levels/level_2.tscn')
+	get_tree().change_scene_to_packed(outside_level)
